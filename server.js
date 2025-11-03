4f9a528a5cacb0294c6df341e5ef759ac6ba22e0
jleoj <84648928+jleoj@users.noreply.github.com>
2025-11-02 17:00:25 -0800

    Update server.js

--- server.js
+++ server.js
@@ -40,10 +40,6 @@ const app = Fastify({
 
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`);
-});
-
 // Enforce HTTPS (redirect HTTP to HTTPS)
 if (process.env.FORCE_HTTPS === "true") {
   app.addHook("onRequest", async (req, reply) => {
